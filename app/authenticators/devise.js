74c3c25958c9fc9ff8fbddad1480f46718ee431c
Andy Brown <brownie3003@gmail.com>
2017-08-05 17:37:46 +0200

    chore: clear up usage of localhost:

--- app/authenticators/devise.js
+++ app/authenticators/devise.js
@@ -1,5 +1,6 @@
 import DeviseAuthenticator from 'ember-simple-auth/authenticators/devise';
+import ENV from '../config/environment';
 
 export default DeviseAuthenticator.extend({
-  serverTokenEndpoint: 'http://localhost:3000/users/sign_in'
+  serverTokenEndpoint: `${ENV.API_HOST}users/sign_in`
 });
