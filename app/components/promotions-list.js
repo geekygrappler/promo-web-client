9cdcbf54b63eb2e7bdb9e8bc9869eb67a05a76ed
Andy Brown <brownie3003@gmail.com>
2017-08-05 16:36:41 +0200

    feat: add promotions-list

---
+++ app/components/promotions-list.js
@@ -0,0 +1,13 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  hasPromotions: computed('promotions', function() {
+    const promotions = this.get('promotions');
+    return promotions.length > 0;
+  }),
+
+  noPromotions: computed('hasPromotions', function() {
+    return !this.get('hasPromotions');
+  })
+});
