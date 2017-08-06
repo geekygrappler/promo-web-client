b3c256481074a1e36940cb9d8e322e47fe7ffab5
Andy <brownie3003@gmail.com>
2017-08-06 18:05:02 +0200

    fix: add / to login

--- app/authenticators/devise.js
+++ app/authenticators/devise.js
@@ -2,5 +2,5 @@ import DeviseAuthenticator from 'ember-simple-auth/authenticators/devise';
 import ENV from '../config/environment';
 
 export default DeviseAuthenticator.extend({
-  serverTokenEndpoint: `${ENV.API_HOST}users/sign_in`
+  serverTokenEndpoint: `${ENV.API_HOST}/users/sign_in`
 });
