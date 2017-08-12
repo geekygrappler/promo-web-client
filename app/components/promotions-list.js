645f30dd7cd7fec75fd5a68f38f3b1b7ad6aaf8e
Andy <brownie3003@gmail.com>
2017-08-12 15:42:51 +0200

    fix: promotions-list not showing all promotions

--- app/components/promotions-list.js
+++ app/components/promotions-list.js
@@ -4,7 +4,7 @@ import { computed } from '@ember/object';
 export default Component.extend({
   hasPromotions: computed('promotions', function() {
     const promotions = this.get('promotions');
-    return promotions.length > 0;
+    return promotions.get('length') > 0;
   }),
 
   noPromotions: computed('hasPromotions', function() {
