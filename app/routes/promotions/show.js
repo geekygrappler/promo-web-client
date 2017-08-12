51ee27fbfe8006ba66d1c35a55fb99ce3abba8f4
Andy <brownie3003@gmail.com>
2017-08-12 18:14:41 +0200

    fix: remove testing .then from model

--- app/routes/promotions/show.js
+++ app/routes/promotions/show.js
@@ -2,10 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.get('store').findRecord('promotion', params.id).then((promotion) => {
-      console.log('hi');
-    }).catch((error) => {
-      console.log(error);
-    });
+    return this.get('store').findRecord('promotion', params.id);
   }
 });
