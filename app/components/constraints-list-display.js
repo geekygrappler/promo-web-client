eddb529468f7989947526babfa39c4d4e585a3d2
Andy <brownie3003@gmail.com>
2017-08-12 17:19:10 +0200

    fixup! fix: TimePeriodConstraint should be PromotionPeriodConstraint

--- app/components/constraints-list-display.js
+++ app/components/constraints-list-display.js
@@ -4,6 +4,6 @@ import { computed } from '@ember/object';
 
 export default Component.extend({
   constraintsWithoutPromotionPeriodConstraint: computed('constraints', function() {
-    return this.get('constraints').filter((constraint) => constraint !== 'TimePeriodConstraint');
+    return this.get('constraints').filter((constraint) => constraint !== 'PromotionPeriodConstraint');
   })
 });
