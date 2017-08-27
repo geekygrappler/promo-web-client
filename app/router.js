53a83b0f8d569ceca017fd93201cbedc9a452c4d
Andy <brownie3003@gmail.com>
2017-08-27 12:29:14 +0200

    feat: how it works route

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('login');
   this.route('sign-up');
+  this.route('how-it-works');
 
   this.route('promotions', function() {
     this.route('draft', function() {
